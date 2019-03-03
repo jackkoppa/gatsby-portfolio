662c2e1b3a12965139adf35ce40ea88efdaa382a
Jack Koppa <hello@jackkoppa.com>
2019-03-03 15:05:59 -0500

    docs, feature: Include 'inspiration' page w/ screens from various publication UIs

---
+++ src/pages/inspiration.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const InspirationPage = ({ data }) => (
+  <Layout>
+    <SEO title="Inspiration" keywords={[`images`, `samples`, `layout`]} />
+    <h1>Inpiration Page</h1>
+    {data.allImageSharp.edges.map(image => {
+        return <Img fluid={image.node.fluid}/>
+    })}
+  </Layout>
+)
+
+export const query = graphql`
+query InspirationPageQuery {
+    allImageSharp(filter: { original: { src: { regex:"/inspiration/"}} }) {
+        edges {
+            node {
+                fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+}
+`
+
+export default InspirationPage
